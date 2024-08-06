632b8993d766d61e078b18c62e7aa72a718b303f
Timur <toombez.timurtokaev@gmail.com>
2024-08-07 00:18:29 +0300

    Add PointImplementation trait and Point struct

--- src/point.rs
+++ src/point.rs
@@ -1,2 +1,27 @@
+use std::ops::{Index, IndexMut};
+
 pub trait PointCoordinate: Clone {}
 impl<T: Clone> PointCoordinate for T {}
+
+pub trait PointImplementation<const DIMENSION: usize, T: PointCoordinate>:
+    Clone
+{
+    fn coordinates(&self) -> Vec<T>;
+
+    fn dimension(&self) -> usize {
+        DIMENSION
+    }
+}
+
+// Base struct
+#[derive(Debug, Clone)]
+pub struct Point<const DIMENSION: usize, T: PointCoordinate> {
+    pub coordinates: Vec<T>,
+}
+
+// Base implementation
+impl <const DIMENSION: usize, T: PointCoordinate> PointImplementation<DIMENSION, T> for Point<DIMENSION, T> {
+    fn coordinates(&self) -> Vec<T> {
+        self.coordinates.clone()
+    }
+}
