f0bdee477b1d5dc8c54907e8b8b02474163ac939
Timur <toombez.timurtokaev@gmail.com>
2025-06-09 12:27:39 +0300

    Add wasm for wolfram crate

--- crates/wolfram_code/src/automaton.rs
+++ crates/wolfram_code/src/automaton.rs
@@ -1,15 +1,25 @@
 use crate::{rule::WolframCodeRule, state::WolframCodeState};
 use toolkit::{
-    lattice::lattice1::Lattice1, neighborhood::nearest::NearestNeighborhoodBuilder1,
-    types::CellularAutomaton,
+    lattice::lattice1::Lattice1,
+    neighborhood::nearest::NearestNeighborhoodBuilder1,
+    types::{CellularAutomaton},
 };
 
+#[cfg(feature = "wasm")]
+use toolkit::types::{BoundaryHandling, BoundaryHandlingLattice, Lattice};
+
+#[cfg(feature = "wasm")]
+use wasm_bindgen::prelude::wasm_bindgen;
+
 #[derive(Debug, Clone)]
+#[cfg_attr(feature = "wasm", wasm_bindgen)]
 pub struct WolframCodeAutomaton {
     rule: WolframCodeRule,
 }
 
+#[cfg_attr(feature = "wasm", wasm_bindgen)]
 impl WolframCodeAutomaton {
+    #[cfg_attr(feature = "wasm", wasm_bindgen(constructor))]
     pub fn new(rule: WolframCodeRule) -> Self {
         Self { rule }
     }
@@ -26,5 +36,46 @@ impl CellularAutomaton for WolframCodeAutomaton {
 
     fn neighborhood_builder(&self) -> Self::NeighborhoodBuilder {
         NearestNeighborhoodBuilder1::new(1)
+    }
+}
+
+#[cfg(feature = "wasm")]
+#[wasm_bindgen(js_name = "WolframCodeLattice")]
+#[derive(Debug, Clone)]
+pub struct WasmWolframCodeLattice {
+    lattice: Lattice1<WolframCodeState>,
+}
+
+#[cfg(feature = "wasm")]
+#[wasm_bindgen(js_class = "WolframCodeLattice")]
+impl WasmWolframCodeLattice {
+    #[wasm_bindgen(constructor)]
+    pub fn new(points: Vec<WolframCodeState>) -> Self {
+        Self {
+            lattice: Lattice1::<WolframCodeState>::from(points),
+        }
+    }
+
+    #[wasm_bindgen]
+    pub fn set_boundary_handing(&mut self, boundary_handling: BoundaryHandling) {
+        self.lattice.set_boundary_handling(boundary_handling);
+    }
+
+    #[wasm_bindgen]
+    pub fn states(&self) -> Vec<WolframCodeState> {
+        self.lattice
+            .points()
+            .iter()
+            .map(|point| self.lattice.get_state(point))
+            .collect()
+    }
+}
+
+#[cfg(feature = "wasm")]
+#[wasm_bindgen]
+impl WolframCodeAutomaton {
+    #[wasm_bindgen(js_name = "step")]
+    pub fn wasm_step(&self, lattice: &mut WasmWolframCodeLattice) {
+        self.step(&mut lattice.lattice);
     }
 }
--- crates/wolfram_code/src/state.rs
+++ crates/wolfram_code/src/state.rs
@@ -1,4 +1,8 @@
+#[cfg(feature = "wasm")]
+use wasm_bindgen::prelude::wasm_bindgen;
+
 #[derive(Debug, Clone, Copy, Default)]
+#[cfg_attr(feature = "wasm", wasm_bindgen)]
 pub enum WolframCodeState {
     #[default]
     Dead,
