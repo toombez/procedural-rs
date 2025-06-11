33cdc6fa0166d58dc0191e2841ab75a61786fdf0
Timur <toombez.timurtokaev@gmail.com>
2025-06-11 17:31:44 +0300

    Add grids

--- apps/web/assets/typescript/utils.ts
+++ apps/web/assets/typescript/utils.ts
@@ -1,9 +1,17 @@
 import type { GraphicsInst } from "vue3-pixi";
 
-function drawSquareCell(graphics: GraphicsInst, x: number, y: number, cellSize: number, color: number) {
+function drawSquareCell(graphics: GraphicsInst, x: number, y: number, cellSize: number, color: number | string) {
     graphics.beginFill(color)
-    graphics.drawRect(x * cellSize, y * cellSize, cellSize, cellSize)
+    graphics.drawRect(x, y, cellSize, cellSize)
     graphics.endFill()
 }
 
-export { drawSquareCell }
+function drawCellBorder(graphics: GraphicsInst, x: number, y: number, borderSize: number, cellSize: number, color: number | string) {
+    drawSquareCell(graphics, x, y, cellSize, color)
+
+    graphics.beginHole()
+    graphics.drawRect(x + borderSize, y + borderSize, cellSize - borderSize * 2, cellSize - borderSize * 2)
+    graphics.endHole()
+}
+
+export { drawSquareCell, drawCellBorder }
---
+++ apps/web/components/Lattice2Grid.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import type { GraphicsInst } from 'vue3-pixi';
+import { drawCellBorder } from '~/assets/typescript/utils';
+
+const props = withDefaults(defineProps<{
+    columns: number,
+    rows: number,
+    cellSize: number,
+    x?: number,
+    y?: number,
+    borderColor: number | string
+    borderSize: number
+}>(), {
+    x: 0,
+    y: 0,
+})
+
+function render(graphics: GraphicsInst) {
+    graphics.clear()
+    for (let y = 0; y < props.rows; y++) {
+        for (let x = 0; x < props.columns; x++) {
+            drawCellBorder(graphics, x * props.cellSize, y * props.cellSize, props.borderSize, props.cellSize, props.borderColor)
+        }
+    }
+}
+</script>
+
+<template>
+    <Graphics
+        @render="render"
+        :x
+        :y
+    />
+</template>
