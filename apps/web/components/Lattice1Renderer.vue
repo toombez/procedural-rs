3c8bd1d9abf5892d8ab47d242a98ebd76c97e9b9
Timur <toombez.timurtokaev@gmail.com>
2025-06-11 14:04:49 +0300

    Add lattice 1 renderer based on lattice 2

---
+++ apps/web/components/Lattice1Renderer.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts" generic="S">
+const props = withDefaults(defineProps<{
+    states: Array<S>,
+    cellSize?: number,
+    x?: number,
+    y?: number,
+    colors: Map<S, number>
+}>(), {
+    cellSize: 1,
+    x: 0,
+    y: 0,
+})
+</script>
+
+<template>
+    <Lattice2Renderer
+        :states="[states]"
+        :cell-size
+        :colors
+        :x
+        :y
+    />
+</template>
